be02e1b242a353cb1c18a3c968847467100c812d
rushiwadkar007 <rwadkar920@gmail.com>
2023-07-05 18:40:29 +0530

    request received filters added.

--- controller/credentialsController.js
+++ controller/credentialsController.js
@@ -25,7 +25,7 @@ const sendProposal = async (req, res) => {
       const proposalBody = {
         auto_remove: true,
         comment: "ECO Credentials Generation Request",
-        connection_id: "89183ec0-1551-4e01-a589-ade99fb97350",
+        connection_id: "ff8cb58a-fe61-4b1d-b6b8-6d548b3f3478",
         credential_preview: {
           "@type": "issue-credential/2.0/credential-preview",
           attributes: [
@@ -59,7 +59,7 @@ const sendProposal = async (req, res) => {
 
       await axios
         .post(
-          "http://172.20.2.139:8089/issue-credential-2.0/send",
+          "http://172.20.2.139:8089/issue-credential-2.0/send-offer",
           proposalBody,
           { headers }
         )
@@ -413,11 +413,110 @@ const getCredOffers = async (req, res) => {
   }
 };
 
+const getCredReceivedRequests = async (req, res) => {
+  try {
+    const { fromDate, toDate, refNO } = req.query;
+    try {
+      const requests = await axios.get(
+        blockchainURL + issueCreds + `?state=request-received`
+      );
+
+      const latestRequests = requests.data.results.sort(function (a, b) {
+        // Turn your strings into dates, and then subtract them
+        // to get a value that is either negative, positive, or zero.
+        return (
+          new Date(b.cred_ex_record.created_at) -
+          new Date(a.cred_ex_record.created_at)
+        );
+      });
+      let pageNumber = req.query.pageNumber;
+      // condition 1 - Data based on date range.
+      if (fromDate && toDate && fromDate !== "" && toDate !== "") {
+        let d1 = fromDate.split("/");
+        let d2 = toDate.split("/");
+        var from = new Date(d1[2], parseInt(d1[1]) - 1, d1[0]); // -1 because months are from 0 to 11
+        var to = new Date(d2[2], parseInt(d2[1]) - 1, d2[0]);
+        const rangeData = latestRequests.filter((item, index) => {
+          let date = new Date(item.cred_ex_record.created_at);
+          const yyyy = date.getFullYear();
+          let mm = date.getMonth() + 1;
+          let dd = date.getDate();
+          let d = `${dd}/${mm}/${yyyy}`;
+          let d3 = d.split("/");
+          let check = new Date(d3[2], parseInt(d3[1]) - 1, d3[0]);
+          if (check > from && check < to) {
+            return item;
+          }
+        });
+        const rangePageSize = rangeData.length / 5;
+        const credRangeData = await paginate(
+          rangeData,
+          rangePageSize,
+          pageNumber
+        );
+        const paginatedRangeCreds =
+          credRangeData.length !== 0
+            ? credRangeData
+            : `Total credential requests ${credData.latestRequests.length} are rendered!`;
+
+        if (refNO) {
+          const refNoData = credRangeData.filter((item) => {
+            return item;
+          });
+          res.status(200).json({
+            data: refNoData,
+            status: "Data Found!",
+          });
+        } else {
+          res.status(200).json({
+            data: paginatedRangeCreds,
+            status: "Data Found!",
+          });
+        }
+      }
+      //condition 2 - Data based latest data
+      else {
+        const pageSize = latestRequests.length / 10;
+        const credData = await paginate(latestRequests, pageSize, pageNumber);
+        const paginatedCreds =
+          credData.length !== 0
+            ? credData
+            : `Total credential requests ${credData.latestRequests.length} are rendered!`;
+
+        if (refNO) {
+          const refNoData = paginatedCreds.filter((item) => {
+            return item;
+          });
+          res.status(200).json({
+            data: refNoData,
+            status: "Data Found!",
+          });
+        } else {
+          res.status(200).json({
+            data: paginatedCreds,
+            status: "Data Found!",
+          });
+        }
+      }
+    } catch (error) {
+      res.status(404).json({
+        data: [],
+        status: "Max Data limit reached!",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      data: null,
+      error: error,
+    });
+  }
+};
 module.exports = {
   sendProposal,
   sendOffer,
   sendRequest,
   issueCredentials,
   getCredentialRequests,
-  getCredOffers
+  getCredOffers,
+  getCredReceivedRequests
 };
